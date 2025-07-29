161277489169fd5fb9beeba72756957c6bd38d9b
raja12aa <80020204+raja12aa@users.noreply.github.com>
2025-07-29 08:46:34 +0100

    Add .next/static/chunks/src_app_contact_page_tsx_9c65fccf._.js 1753775193583

---
+++ .next/static/chunks/src_app_contact_page_tsx_9c65fccf._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_56bb8a71._.js"
+],
+    source: "dynamic"
+});
