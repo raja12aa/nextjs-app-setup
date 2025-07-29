3766ad69b29493f6138d823e96fce5cb7d61e26a
raja12aa <80020204+raja12aa@users.noreply.github.com>
2025-07-29 08:47:06 +0100

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1753775225918

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__4572c8f9._.css",
+  "static/chunks/node_modules_1c06f25b._.js",
+  "static/chunks/src_cc5afc96._.js"
+],
+    source: "dynamic"
+});
